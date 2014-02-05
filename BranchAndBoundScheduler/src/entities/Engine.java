e11631c1d9172976efae512812930d245673e806
Benjamin Kneebone <bk8g11@soton.ac.uk>
2014-02-05 14:01:28 +0000

    ADD: Crude method to calculate stopping distance for an engine

--- BranchAndBoundScheduler/src/entities/Engine.java
+++ BranchAndBoundScheduler/src/entities/Engine.java
@@ -8,6 +8,7 @@ public class Engine {
 	private int driForce;		//Newtons
 	private int braForce;       //Newtons
 	private double acceleration;//m/s^2
+	private double deceleration;//m/s^2
 	private String name;
 	private int[] speedProfile = new int[10];
 	
@@ -19,6 +20,7 @@ public class Engine {
 		this.braForce = breForce;
 		this.name = name;
 		this.acceleration = (double) driForce / weight;
+		this.deceleration = (double) braForce / weight;
 		this.speedProfile = speedProfile;
 	}
 	
@@ -46,14 +48,7 @@ public class Engine {
 		this.speedProfile = speedProfile;
 	}
 	
-	public void timeToTraverse(Block block, int speed){
-		
-		
-		
-		
-		
-		
-	}
+
 	
 	public double distanceToFullSpeed(int speed){
 		
@@ -67,6 +62,18 @@ public class Engine {
 		return (((double) vsquared - usquared) / ((double) 2 * acceleration));
 	}
 	
+	public double distanceToStop(int speed){
+		
+		//Final speed m/s
+		double vsquared = Math.pow((double) (speed * 1000) / 3600, 2);
+				
+		//Initial speed m/s
+		double usquared = Math.pow(0, 2);
+				
+		//Distance in m
+		return (((double) vsquared - usquared) / ((double) 2 * deceleration));
+	}
+	
 	
 	public void printDetails(){
 		System.out.println(name + " profile:");
@@ -75,6 +82,7 @@ public class Engine {
 		System.out.println("Driving Force: " + driForce);
 		System.out.println("Braking Force: " + braForce);
 		System.out.println("Acceleration: " + acceleration);
+		System.out.println("Deceleration: " + deceleration);
 		System.out.print("Speed Profile: ");
 		for(int x = 0; x < 10; x++)
 			System.out.print(speedProfile[x] + " ");

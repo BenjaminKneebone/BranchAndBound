e77b28841464220a098dedfe4caf3191eb986f27
Benjamin Kneebone <bk8g11@soton.ac.uk>
2014-02-05 18:19:58 +0000

    ADD: Methods to work out time for train to traverse block, not complete

--- BranchAndBoundScheduler/src/entities/Engine.java
+++ BranchAndBoundScheduler/src/entities/Engine.java
@@ -48,53 +48,102 @@ public class Engine {
 		this.speedProfile = speedProfile;
 	}
 	
-	public double timeToTraverse(Block block, int speed){
+	/**Calculate traversal providing the mimumum amount of the time the train must spend in the block
+	 * @param block - block to be traversed
+	 * @param speed - entry speed
+	 * @param time - minimum time to spend in bloc
+	 * @return BlockExit object containing time to traverse block and exit speed
+	 */
+	public BlockExit delayTraversal(Block block, double speed, double time){
+			
+		double acc = (2 * (block.getLength() - ((speed/3.6) * time))) / Math.pow(time,2);
+		System.out.println("acc: " + acc);
 		
+		double finalVel = ((2 * block.getLength())/time) - (speed/3.6);
+		
+		return new BlockExit(time, finalVel);
+		
+	}
+	
+	/**Returns information about a train traversing a block if at full power
+	 * @param block - block to be traversed
+	 * @param speed - entry speed (km/h)
+	 * @return BlockExit object containing time to traverse block and exit speed (km/h)
+	 */
+	public BlockExit timeToTraverse(Block block, double speed){
+		
+		//Not enough time to reach full speed
 		if(distanceToFullSpeed(speed) > block.getLength()){
 			
+			//The final velocity achievable over the distance (sqrt(u^2 - 2as))
+			double finalVel = Math.sqrt(Math.pow((speed / 36), 2) + (2 * acceleration * block.getLength()));
+	
+			//The time it will take to reach this velocity (hence time in the block) ((v-u) / a)
+			double time = (finalVel - (speed/ 3.6)) / acceleration;
 			
-			return 0;
+			//convert to km
+			finalVel = (finalVel * 3.6);
+			
+			return new BlockExit(time, finalVel);
 		}else{
 			
+			//distance left after getting to full speed
 			double distanceAtFullSpeed = block.getLength() - distanceToFullSpeed(speed);
-			double totalTime = timeToFullSpeed(speed) + (distanceAtFullSpeed/ (speedProfile[9] / 3600));
-			return totalTime;
-		}
-		
-		
-		
-		
-		
+			
+			//Time to accelerate to full speed and to cover remaining distance at full speed
+			double totalTime = timeToFullSpeed(speed) + (distanceAtFullSpeed/ ((double) speedProfile[9] / 3.6));
+			
+			//Leaving at full speed
+			return new BlockExit(totalTime, speedProfile[9]);
+		}	
 	}
 	
-	public double timeToFullSpeed(int speed){
-
-		return (speedProfile[9] - speed) / acceleration;
+	/**
+	 * 
+	 * @param speed - Speed of train
+	 * @return time taken to reach full speed (in seconds)
+	 */
+	public double timeToFullSpeed(double speed){
+		//Difference in speed (m/s) divided by acceleration (m/s/s)
+		return ((speedProfile[9]/3.6) - (speed/3.6)) / acceleration;
 	}
 
-	
-	public double distanceToFullSpeed(int speed){
+	/**
+	 * @param speed - Speed of train
+	 * @return distance needed to reach full speed (in metres)
+	 */
+	public double distanceToFullSpeed(double speed){
 		
 		//Final speed m/s
-		double vsquared = Math.pow((double) (speedProfile[9] * 1000) / 3600, 2);
+		double vsquared = Math.pow((double) (speedProfile[9] / 3.6), 2);
 		
 		//Initial speed m/s
-		double usquared = Math.pow((double) (speed * 1000) / 3600, 2);
+		double usquared = Math.pow((double) (speed / 3.6), 2);
 		
-		//Distance in m
+		//Distance in m (v2 - u2 / 2a)
 		return (((double) vsquared - usquared) / ((double) 2 * acceleration));
 	}
 	
-	public double distanceToStop(int speed){
+	/**
+	 * @param speed - Speed of train (Km/h)
+	 * @return time needed to stop train (in metres)
+	 */
+	public double timeToStop(double speed){
+		//Difference in speed (m/s) divided by acceleration (m/s/s)
+		return (speed/3.6) / deceleration;
+	}
+	
+	/**
+	 * @param speed - Speed of train
+	 * @return distance needed to stop train (in metres)
+	 */
+	public double distanceToStop(double speed){
 		
-		//Final speed m/s
-		double vsquared = Math.pow((double) (speed * 1000) / 3600, 2);
-				
 		//Initial speed m/s
-		double usquared = Math.pow(0, 2);
-				
-		//Distance in m
-		return (((double) vsquared - usquared) / ((double) 2 * deceleration));
+		double vsquared = Math.pow((double) (speed / 3.6), 2);
+								
+		//Distance in m (v2 - u2 / 2a) - u = 0
+		return ((double) vsquared / ((double) 2 * deceleration));
 	}
 	
 	

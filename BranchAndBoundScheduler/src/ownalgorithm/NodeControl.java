6fde2349c1473154e6a8d1eea57c1c3cca65ce9b
Benjamin Kneebone <bk8g11@soton.ac.uk>
2014-04-11 19:55:12 +0100

    New algorithm started

--- BranchAndBoundScheduler/src/ownalgorithm/NodeControl.java
+++ BranchAndBoundScheduler/src/ownalgorithm/NodeControl.java
@@ -3,6 +3,7 @@ package ownalgorithm;
 import java.util.ArrayList;
 
 import entities.BlockOccupation;
+import entities.Engine;
 import entities.Journey;
 
 public class NodeControl {
@@ -21,10 +22,69 @@ public class NodeControl {
 		for(int x = 0; x < numBlocks; x++)
 			occupied.add(new ArrayList<BlockOccupation>());		
 		
+		
+		schedule();
+		
+		
 	}
 	
 	
+	private void schedule(){
+		
+		Engine train;
+		
+		for(Journey j: journeys){
+		
+			train = j.getTrain();
+			
+			for(BlockOccupation b: j.getBlockOccupations()){
+			
+				ArrayList<BlockOccupation> blockOccupied = occupied.get(b.getBlock().getID());
+				
+				int x = 0;
+				boolean scheduled = false;
+				
+				while(!scheduled){
+				
+					while(true){
+						//If time is during occupation x (Invalid arrival time)
+						if(isTimeCollision(b.getArrTime(), blockOccupied.get(x))){
+							//Not last block, back to back occupations in block
+							if(x < blockOccupied.size() - 1 &&
+									b.getArrTime() == blockOccupied.get(x+1).getArrTime()){
+										//Iterate
+										x++;
+										continue;
+							}else
+									//Push back arrival time
+									b.setArrTime(blockOccupied.get(x).getDepTime());
+						}
+				
+						break;
+					}
+				
+					b = 
+		
+					//Do scheduling
+		
+				
+					
+					
+					
+					
+					
+					
+				}
+				
+			}
+			
+		}
+		
+	}
 	
+	private boolean isTimeCollision(double time, BlockOccupation blockOccupation){
+		return time >= blockOccupation.getArrTime() && time < blockOccupation.getDepTime();
+	}
 	
 	
 	
